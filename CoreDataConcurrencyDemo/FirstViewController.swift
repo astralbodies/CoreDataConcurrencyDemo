f4e488f1f09644185676b6ac9af6764fa90442da
Aaron Douglas <astralbodies@gmail.com>
2014-08-03 18:13:20 -0500

    Initial Commit

---
+++ CoreDataConcurrencyDemo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CoreDataConcurrencyDemo
+//
+//  Created by Aaron Douglas on 8/3/14.
+//  Copyright (c) 2014 Automattic Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
